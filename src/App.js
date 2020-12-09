40f8fb2865b5178ead13cca37efce74123e3a70e
ryad <ryadallaf@gmail.com>
2020-12-10 00:47:35 +0100

    config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import store from "./redux/store/combineStore";
+import FilterProfileView from "./components/FilterProfile/container/FilterProfileView";
+import ProfileDetailsView from "./components/ViewProfile/container/ProfileDetailsView";
+
 
 function App() {
-  return (
-    <div className="App">
-      GitHub-profile
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={FilterProfileView} />
+
+                    <Route exact path="/profile" component={ProfileDetailsView} />
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.github.com/users';
+export const clientId = 'Iv1.3257a9819dfbb01e';
+
+export const clientSecret = 'f5f4e39e3190e9c1f9d04c829306d3dca9f7679f';
